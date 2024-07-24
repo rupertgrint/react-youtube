e35bbb41b07494693a86df14b1fc8b089a5453a9
rupertgrint <joorohi@gmail.com>
2024-07-24 11:20:44 +1000

    fix: remove unused images, change Navbar to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { CiSearch } from 'react-icons/ci';
 import { useSearch } from '../context/SearchContext';
 
-export default function Navbar() {
+export default function Header() {
   const { searchInput, setSearchInput } = useSearch();
   const navigate = useNavigate();
 
@@ -16,9 +16,13 @@ export default function Navbar() {
   };
 
   return (
-    <nav className='flex flex-row justify-center my-[50px] gap-2'>
+    <header className='flex flex-row justify-center my-[50px] gap-2'>
       <Link to='/'>
-        <img className='h-8' src='/yt_logo_rgb_dark.png' alt='Youtube Logo' />
+        <img
+          className='h-8'
+          src='/images/yt_logo_rgb_dark.png'
+          alt='Youtube Logo'
+        />
       </Link>
       <form onSubmit={handleSubmit}>
         <input
@@ -32,6 +36,6 @@ export default function Navbar() {
       <button type='submit'>
         <CiSearch className='w-6 h-6 border-none cursor-pointer hover:scale-[110%] transition' />
       </button>
-    </nav>
+    </header>
   );
 }
